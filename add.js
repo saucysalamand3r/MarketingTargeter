5df24d022cdc2466e4b3141e7e0dce84834cb33a
saucysalamand3r <saucysalamand3r@protonmail.com>
2019-04-21 21:40:36 -0400

    Switched to hashing by 7

--- add.js
+++ add.js
@@ -21,7 +21,7 @@ function writePerson(fName, lName, age, gender, phone, email, interest){
         email: email,
         interest: interest,
     }
-    localStorage.setItem(Date.now()-(Math.floor(Date.now()/1000000)), JSON.stringify(person));
+    localStorage.setItem(Date.now()%7, JSON.stringify(person));
 }
 
 inp.onclick = function(){
--- find.js
+++ find.js
@@ -20,7 +20,7 @@ function display(person){
 }
 
 function search(term){
-    for(i=Date.now()-(Math.floor(Date.now()/1000000));i>5000;i--){
+    for(i=Date.now()%7;i>0;i--){
         const ithPerson = localStorage.getItem(i);
         if(ithPerson != null){
             if (ithPerson.indexOf(term) != -1) {
